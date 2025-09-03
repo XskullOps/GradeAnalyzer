d7dd0fbd732ab79d2226f3b3f505577d41671cc8
XskullOps <saisridhar.v.007@gmail.com>
2025-09-03 13:53:16 -0500

    Few changes to the code

--- grades.py
+++ grades.py
@@ -3,10 +3,10 @@ def average_score(scores:list[int])->float:
         return 0.0
     return sum(scores) / len(scores)
 
-def pass_fail(score:int)->int:
+def pass_fail(score:int)->str:
     return "pass" if score >= 60 else "fail"
 
-def analyze_scores(scores:list[int])->float:
+def analyze_scores(scores:list[int])->None:
     for i in scores:
         print(f"Score {i} -> {pass_fail(i)}")
         
